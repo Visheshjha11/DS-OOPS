858843edd722bdbcc51835d270bca1aa592a3e7a
Visheshjha11 <visheshjha456@gmail.com>
2025-09-12 09:54:55 +0530

    loops lecture 1

---
+++ src/loops.java
@@ -0,0 +1,38 @@
+public class loops {
+
+//    public static void main(String[] args) {
+//
+//        //comments
+//        System.out.print("Let's start Java!!\n");
+//        System.out.println("i am pretty exited");//using println is same as using \n
+//        System.out.print("hello bitch\n");
+//        System.out.println("intellij is pretty good");//sout is shortcut in intellij for System.out.print()
+//    }
+        // numbers from 0 to 1
+
+        public static void main(String[] args){
+//        for(int i= 1; i<=10; i++){
+//            if(i<=9) {
+//                System.out.print(i + ", ");
+//            }
+//            else {
+//                System.out.print(i+".");
+//            }
+//        }
+//        System.out.println();
+//        int j = 1;
+//        while(j<=10){
+//
+//            System.out.println(j);
+//            j++;
+//        }
+//        System.out.println(" ");
+            for(int i=1; i<=10; i++){
+                System.out.println("2"+"X"+i+" = "+ 2*i);
+
+            }
+        }
+
+
+
+}
